e01f6224af1157801fcd74b55d3189e611abcf14
ohmijin <mijinoh8745@gmail.com>
2018-04-05 16:05:44 +0900

    commit successed

--- src/main/scala/com/mi/joinEx.scala
+++ src/main/scala/com/mi/joinEx.scala
@@ -17,13 +17,7 @@ object joinEx {
     var subData = spark.read.format("csv").
       option("header", "true").load(dataPath + subFile)
 
-    //dataframe
-    var mainDataDf = spark.read.format("csv").
-      option("header","true").load(dataPath + subData)
 
-    //dataframe
-    var subDataDf = spark.read.format("csv").
-      option("header","true").load(dataPath + subData)
 
 
     mainData.createOrReplaceTempView("maindata")
@@ -32,5 +26,72 @@ object joinEx {
     var leftJoinData = spark.sql("select a.*, b.productname " +
       "from maindata a left outer join subdata b " +
       "on a.productgroup = b.productid")
+
+    var innerJoinData = spark.sql("select a.*, b.productname " +
+      "from maindata a inner join subdata b " +
+      "on a.productgroup = b.productid")
+
+    //데이터 프레임에 담아보자
+    //dataframe
+    var mainDataDf = spark.read.format("csv").
+      option("header","true").load(dataPath + subData)
+
+    //dataframe
+    var subDataDf = spark.read.format("csv").
+      option("header","true").load(dataPath + subData)
+
+    mainDataDf.createOrReplaceTempView("mainTable")
+    subDataDf.createOrReplaceTempView("subTable")
+
+    //곱하기가 된다
+    spark.sql("select a.regionid, b.productgroup, a.yearweek, a.qty "  +
+    "from mainTable a " + "left join subTable b "
+    )
+
+    var leftJoinDataDf = spark.sql("select a.regionid, b.productgroup, a.yearweek, a.qty" +
+      "from mainTable a " + "left join subTable b "+
+      "on a.productgroup = b.productid")
+
+    var innerJoinDataDf = spark.sql("select a.regionid, b.productgroup, a.yearweek, a.qty" +
+      "from mainTable a " + "inner join subTable b "+
+      "on a.productgroup = b.productid")
+
+    //오라클 접속 DB 두개를 조인해보자
+
+    var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+    var staticUser = "kopo"
+    var staticPw = "kopo"
+    var selloutDb1 = "kopo_channel_seasonality_new"
+    var selloutDb2 = "kopo_region_mst"
+
+    // jdbc (java database connectivity) 연결
+    val oracleSeasonData= spark.read.format("jdbc").
+      options(Map("url" -> staticUrl,"dbtable" -> selloutDb1,"user" -> staticUser, "password" -> staticPw)).load
+
+    val oracleRegionData= spark.read.format("jdbc").
+      options(Map("url" -> staticUrl,"dbtable" -> selloutDb2,"user" -> staticUser, "password" -> staticPw)).load
+
+    // 메모리 테이블 생성
+    oracleSeasonData.createOrReplaceTempView("oracleSeasonTable")
+    oracleSeasonData.show
+
+    oracleRegionData.createOrReplaceTempView("oracleRegionTable")
+    oracleRegionData.show
+
+    var innerJoinDataEx = spark.sql("select a.*, b.* " +
+      "from oracleRegionTable a inner join oracleSeasonTable b " +
+      "on a.regionid = b.regionid")
+
+    println(innerJoinDataEx.show(5))
+
+    var leftJoinDataEx = spark.sql("select a.*, b.* " +
+      "from oracleRegionTable a left join oracleSeasonTable b " +
+      "on a.regionid = b.regionid")
+
+    println(leftJoinDataEx.show(5))
+
+    println(innerJoinDataEx.count)
+    println(leftJoinDataEx.count)
   }
+
 }
